70edad01baea9ee0a7da24669a3b8f386118b769
leroyjps <160677008+leroyjps@users.noreply.github.com>
2024-10-08 12:00:10 -0300

    Update buscadorPedidos.js

--- netlify/functions/buscadorPedidos.js
+++ netlify/functions/buscadorPedidos.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 exports.handler = async (event, context) => {
     try {
+        // Estabelecendo a conexão com o banco de dados
         const connection = await mysql.createConnection({
             host: process.env.DB_HOST,
             user: process.env.DB_USER,
@@ -10,15 +11,21 @@ exports.handler = async (event, context) => {
             database: process.env.DB_NAME
         });
 
+        // Executando a consulta SQL para buscar os pedidos
         const [rows] = await connection.execute('SELECT * FROM pedidos ORDER BY dataEntrada DESC');
-        connection.end();
+        
+        // Fechando a conexão com o banco de dados de forma segura
+        await connection.end();
 
+        // Retornando os pedidos em formato JSON
         return {
             statusCode: 200,
             body: JSON.stringify(rows),
         };
     } catch (error) {
         console.error('Erro ao buscar pedidos:', error);
+
+        // Caso ocorra algum erro, retornamos uma resposta de erro
         return {
             statusCode: 500,
             body: JSON.stringify({ message: 'Erro ao buscar pedidos' }),
